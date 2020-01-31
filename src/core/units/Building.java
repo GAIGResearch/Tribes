2f1e69460fe6ee4dc35aecbc35dfce2b395c90f3
Yu-Jhen Hsu <rovenr32@gmail.com>
2020-01-31 13:43:37 +0000

    update building functions (allows other classes get the function based on building class)

--- src/core/units/Building.java
+++ src/core/units/Building.java
@@ -1,6 +1,7 @@
 package core.units;
 
 import core.Types;
+import core.game.Board;
 
 public abstract class Building {
 
@@ -24,5 +25,25 @@ public abstract class Building {
 
     public Building copy(){return null;}
 
-    // TODO: Linked to BOARD to see if it make requirements to build (return boolean) -> Implement this method to all buildings
+    public Types.BUILDING getTYPE() {
+        return null;
+    }
+
+    public int getCOST() {
+        return -1;
+    }
+
+    public int getPRODUCTION() {
+        return -1;
+    }
+
+    public Types.TERRAIN getTERRAIN_CONSTRAINT() {
+        return null;
+    }
+
+    public Types.RESOURCE getRESOURCE_CONSTRAINT(){return null;}
+
+    public boolean is_buildable(Board board){
+        return board.getTerrainAt(x, y).equals(getTERRAIN_CONSTRAINT()) && board.getResourceAt(x, y).equals(getRESOURCE_CONSTRAINT());
+    }
 }

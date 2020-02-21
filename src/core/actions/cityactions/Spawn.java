5757f229eb4e0b7034dbf6707a047b43f328ad91
yujhen <rovenr32@gmail.com>
2020-02-21 20:27:25 +0000

    Update Spawn Action

--- src/core/actions/cityactions/Spawn.java
+++ src/core/actions/cityactions/Spawn.java
@@ -1,9 +1,13 @@
 package core.actions.cityactions;
 
+import core.TechnologyTree;
+import core.TribesConfig;
 import core.Types;
 import core.actions.Action;
+import core.actors.Tribe;
 import core.game.GameState;
 import core.actors.City;
+import utils.Vector2d;
 
 import java.util.LinkedList;
 
@@ -23,19 +27,44 @@ public class Spawn extends CityAction
 
     @Override
     public LinkedList<Action> computeActionVariants(GameState gs) {
-        //TODO: compute spawn actions
-        return null;
+        LinkedList<Action> actions = new LinkedList<>();
+
+        Tribe tribe = gs.getTribe(city.getTribeId());
+        TechnologyTree t = tribe.getTechTree();
+        int star = tribe.getStars();
+        if (gs.getBoard().getUnitAt(city.getX(), city.getY()) == null){
+            for(Types.UNIT unit: Types.UNIT.values()){
+                if (unit.getCost() <= star && unit.getKey() <= 7){
+                    if (unit.getRequirement() == null){
+                        Spawn newAction = new Spawn(city);
+                        newAction.setUnitType(unit);
+                        actions.add(newAction);
+                    }else if(t.isResearched(unit.getRequirement())){
+                        Spawn newAction = new Spawn(city);
+                        newAction.setUnitType(unit);
+                        actions.add(newAction);
+                    }
+                }
+            }
+        }
+        return actions;
     }
 
     @Override
     public boolean isFeasible(GameState gs) {
-        //TODO: Is feasible spawn
-        return false;
+        boolean costRequirement =  gs.getTribe(city.getTribeId()).getStars() >= unit_type.getCost();
+        boolean techniqueRequirement = true;
+        if (unit_type.getRequirement() != null){
+            techniqueRequirement = gs.getTribe(city.getTribeId()).getTechTree().isResearched(unit_type.getRequirement());
+        }
+        return costRequirement && techniqueRequirement;
     }
 
     @Override
     public boolean execute(GameState gs) {
-        //TODO: execute a spawn action.
-        return false;
+        if (isFeasible(gs)){
+            unit_type.createUnit(new Vector2d(city.getX(), city.getY()), 0, false, city.getActorID(), city.getTribeId(), unit_type);
+        }
+        return true;
     }
 }

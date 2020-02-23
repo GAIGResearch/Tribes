a49e21dedf0e6873a1ceff1b370652bb2abc5288
Bobby <Bobby.Khaleque@gmail.com>
2020-02-23 15:27:41 +0000

    Completed ResourceGathering class

--- src/core/actions/cityactions/ResourceGathering.java
+++ src/core/actions/cityactions/ResourceGathering.java
@@ -2,6 +2,7 @@ package core.actions.cityactions;
 
 import core.Types;
 import core.actions.Action;
+import core.game.Board;
 import core.game.GameState;
 import core.actors.City;
 
@@ -35,13 +36,57 @@ public class ResourceGathering extends CityAction
     @Override
     public boolean isFeasible(final GameState gs)
     {
-        //TODO: isFeasible ResourceGathering
+        Board b = gs.getBoard();
+        // Check if resource in range
+        for(int x = this.city.getX()- this.city.getBound(); x < x+ this.city.getBound(); x++){
+            for(int y = this.city.getX()- this.city.getBound(); y < y+ this.city.getBound(); y++){
+                if(b.getResourceAt(x,y) == this.resource){
+                    switch (this.resource){
+                        case FISH:
+                            if(b.getTribe(this.city.getTribeId()).getTechTree().isResearched(Types.TECHNOLOGY.FISHING))
+                                return true;
+                            else
+                                return false;
+                        case ORE:
+                            if(b.getTribe(this.city.getTribeId()).getTechTree().isResearched(Types.TECHNOLOGY.MINING))
+                                return true;
+                            else
+                                return false;
+                            case WHALES:
+                            if(b.getTribe(this.city.getTribeId()).getTechTree().isResearched(Types.TECHNOLOGY.WHALING))
+                                return true;
+                            else
+                                return false;
+                    }
+                }
+                    return true;
+                }
+            }
         return false;
     }
 
+
     @Override
     public boolean execute(GameState gs) {
-        //TODO: execute resource gathering
+        //Check if action feasible before execution
+        if(isFeasible(gs)){
+            switch (this.resource){
+                case CROPS:
+                case FISH:
+                case ANIMAL:
+                case FRUIT:
+                    this.city.addExtraStar(2);
+                    return true;
+                case ORE:
+                case WHALES:
+                    this.city.addExtraStar(5);
+                    return true;
+                case RUINS:
+                    this.city.addExtraStar(10);
+                    return true;
+            }
+
+        }
         return false;
     }
 }

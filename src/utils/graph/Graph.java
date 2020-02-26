067d1198ca9b3eef06dc0c394dd604fb04a78ed5
GAIGResearch <diego.perez@qmul.ac.uk>
2020-02-26 16:07:24 +0000

    Partial graph

---
+++ src/utils/graph/Graph.java
@@ -0,0 +1,21 @@
+package utils.graph;
+
+import java.util.ArrayList;
+
+public class Graph {
+
+    private ArrayList<Node> nodes;
+
+    public Graph()
+    {
+        nodes = new ArrayList<>();
+    }
+
+    public void addNode()
+    {
+
+    }
+
+
+
+}
---
+++ src/utils/graph/Node.java
@@ -0,0 +1,6 @@
+package utils.graph;
+
+public class Node<T>
+{
+    private T id;
+}

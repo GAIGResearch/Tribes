f40bda463218833b11775d27d9318a6987cfc1ac
diegopliebana <diego.perez@qmul.ac.uk>
2020-02-26 16:24:06 +0000

    Graph and node classes

--- src/utils/graph/Graph.java
+++ src/utils/graph/Graph.java
@@ -1,20 +1,53 @@
 package utils.graph;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class Graph {
+public class Graph<T> {
 
-    private ArrayList<Node> nodes;
+    private HashMap<T, Node> nodes;
+    private Node mainNode;
 
     public Graph()
     {
-        nodes = new ArrayList<>();
+        nodes = new HashMap<>();
     }
 
-    public void addNode()
+    public void addNode(T id)
     {
+        Node n = new Node<T>(id);
+        nodes.put(id, n);
 
+        if(nodes.size() == 1)
+            mainNode = n;
     }
+
+    public void addEdge(T id0, T id1)
+    {
+        Node n0 = nodes.get(id0);
+        Node n1 = nodes.get(id1);
+        if(n0==null || n1==null)
+        {
+            System.out.println("WARNING: nodes not found for adding edge from "
+                    + id0 + " (" + n0 + ") to " + id1 + " (" + n1 + ")");
+            return;
+        }
+        n0.addNeighbour(n1);
+    }
+
+    public void removeEdge(T id0, T id1)
+    {
+        Node n0 = nodes.get(id0);
+        Node n1 = nodes.get(id1);
+        if(n0==null || n1==null)
+        {
+            System.out.println("WARNING: nodes not found for removing edge from "
+                    + id0 + " (" + n0 + ") to " + id1 + " (" + n1 + ")");
+            return;
+        }
+        n0.removeNeighbour(n1);
+    }
+
 
 
 
--- src/utils/graph/Node.java
+++ src/utils/graph/Node.java
@@ -1,6 +1,25 @@
 package utils.graph;
 
+import java.util.ArrayList;
+
 public class Node<T>
 {
     private T id;
+    private ArrayList<Node> neighbours;
+
+    public Node (T id)
+    {
+        this.id = id;
+        this.neighbours = new ArrayList<>();
+    }
+
+    public void addNeighbour(Node neigh)
+    {
+        this.neighbours.add(neigh);
+    }
+
+    public void removeNeighbour(Node neigh)
+    {
+        this.neighbours.remove(neigh);
+    }
 }

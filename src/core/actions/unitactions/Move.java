3304a99f5a526b7eb8accffdf1e43e561c526e12
Raluca D. Gaina <r.d.gaina@qmul.ac.uk>
2020-03-03 11:52:38 +0000

    Didn't mean to push this

--- src/core/actions/unitactions/Move.java
+++ src/core/actions/unitactions/Move.java
@@ -26,19 +26,6 @@ public class Move extends UnitAction
 
     @Override
     public LinkedList<Action> computeActionVariants(final GameState gs) {
-        LinkedList<Action> actions = new LinkedList<>();
-        Move act = new Move(super.unit);
-        act.setDest(unit.getPosition().x-1, unit.getPosition().y);
-        actions.add(act);
-        act = new Move(super.unit);
-        act.setDest(unit.getPosition().x, unit.getPosition().y-1);
-        actions.add(act);
-        act = new Move(super.unit);
-        act.setDest(unit.getPosition().x+1, unit.getPosition().y);
-        actions.add(act);
-        act = new Move(super.unit);
-        act.setDest(unit.getPosition().x, unit.getPosition().y+1);
-        actions.add(act);
         //TODO: compute all the possible Move actions for super.unit.
         return actions;
     }
@@ -53,7 +40,6 @@ public class Move extends UnitAction
     @Override
     public boolean execute(GameState gs) {
         //TODO Execute this Move action
-        unit.setPosition(destX, destY);
         return true;
     }
 }

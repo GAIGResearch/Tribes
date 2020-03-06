ad1608c8c80036d95771e2aee91c250f0a1d008b
yujhen <rovenr32@gmail.com>
2020-03-06 10:58:37 +0000

    Update shift population

--- src/core/actions/unitactions/Capture.java
+++ src/core/actions/unitactions/Capture.java
@@ -1,5 +1,6 @@
 package core.actions.unitactions;
 
+import core.Types;
 import core.actions.Action;
 import core.actors.Tribe;
 import core.game.Board;
@@ -9,6 +10,7 @@ import core.actors.units.Unit;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Random;
 
 public class Capture extends UnitAction
 {
@@ -59,6 +61,30 @@ public class Capture extends UnitAction
         // Change city tribe id to execute action
         Board b = gs.getBoard();
         Tribe t = b.getTribe(this.unit.getTribeId());
-        return b.capture(t,this.targetCity.getPosition().x,this.targetCity.getPosition().y);
+        Types.TERRAIN ter = gs.getBoard().getTerrainAt(targetCity.getPosition().x, targetCity.getPosition().y);
+        boolean isVillage = ter == Types.TERRAIN.VILLAGE;
+        boolean isCapture =  b.capture(t,this.targetCity.getPosition().x,this.targetCity.getPosition().y);
+        if (isCapture && isVillage){
+            City capital = (City) gs.getActor(t.getCapitalID());
+            if (capital.getPopulation() > 0){
+                capital.subtractPopulation(1);
+            }else{
+                ArrayList<Integer> citiesId = t.getCitiesID();
+                citiesId.remove((Integer)t.getCapitalID());
+                //TODO: Using Random
+                Random r = new Random();
+                while (citiesId.size() > 0){
+                    int index = r.nextInt(citiesId.size());
+                    City c = (City) gs.getActor(citiesId.get(index));
+                    if (c.getPopulation() > 0){
+                        c.subtractPopulation(1);
+                        break;
+                    }
+                    citiesId.remove(index);
+                }
+                // TODO: Issue: What if every city's population is 0
+            }
+        }
+        return isCapture;
     }
 }

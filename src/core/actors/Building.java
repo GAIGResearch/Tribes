05d50190f4a983526db0c13f401bbaf3a7524dea
diegopliebana <diego.perez@qmul.ac.uk>
2020-03-20 21:41:54 +0000

    Adjacency constraints added to Build Action
    Also, all building classes disappear, only having one class, Building, with its type.

---
+++ src/core/actors/Building.java
@@ -0,0 +1,26 @@
+package core.actors;
+
+import core.Types;
+import utils.Vector2d;
+
+public class Building {
+
+    public Vector2d position;
+    public  Types.BUILDING type;
+
+    public Building(int x, int y, Types.BUILDING type) {
+        this.position = new Vector2d(x, y);
+        this.type = type;
+    }
+
+    public Building copy()
+    {
+        return new Building(position.x, position.y, type);
+    }
+
+    public int getBonus(){
+        return type.getBonus();
+    }
+    public int getPoints() {return type.getPoints();}
+
+}

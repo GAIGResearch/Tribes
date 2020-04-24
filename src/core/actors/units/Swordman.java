3db915d2c2a407db4808b438df88337302270a80
GAIGResearch <35296018+GAIGResearch@users.noreply.github.com>
2020-04-24 16:28:10 +0100

    That return statement should've been kept

--- src/core/actors/units/Swordman.java
+++ src/core/actors/units/Swordman.java
@@ -22,6 +22,6 @@ public class Swordman extends Unit
         c.setCurrentHP(getCurrentHP());
         c.setActorId(getActorId());
         c.setStatus(getStatus());
-        return c;
+        return hideInfo ? (Swordman) c.hide() : c;
     }
 }

a0a4a47d21d29f6c6b7c6be83a2be3fc6eca5de6
diegopliebana <diego.perez@qmul.ac.uk>
2020-05-21 17:23:13 +0100

    tribe now identified in the level

--- src/core/game/LevelLoader.java
+++ src/core/game/LevelLoader.java
@@ -31,7 +31,7 @@ class LevelLoader
         // Dimensions of the level read from the file.
         size.width = lines.length;
         size.height = lines.length;
-        int tribeID = 0;
+        int tribesFound = 0;
         int numTribes = tribes.length;
 
         board.init(size.width, tribes);
@@ -48,36 +48,40 @@ class LevelLoader
                 // Retrieve the chars and assign the corresponding enum values in the board.
                 String[] tileSplit = tile[j].split(":");
                 char terrainChar = tileSplit[0].charAt(0);
+                boolean isCity = terrainChar == Types.TERRAIN.CITY.getMapChar();
 
-                if(terrainChar == Types.TERRAIN.CITY.getMapChar())
+                if(isCity)
                 {
-                    if(tribeID==numTribes)
+                    if(tribesFound==numTribes)
                     {
-                        //If we've already allocated all the cities to the number of tribes, turn this
-                        //extra city into a village.
+                        //If we've already allocated all the cities to the number of tribes, turn this extra city into a village.
                         terrainChar = Types.TERRAIN.VILLAGE.getMapChar();
-                    }else
-                    {
-                        //A city to create. Add it and assign it to the next tribe.
-                        City c = new City(i, j, tribeID);
-                        c.setCapital(true);
-                        board.addCityToTribe(c,rnd);
-
-                        //Also, each tribe starts with a unit in the same location where the city is
-                        Types.UNIT unitType = tribes[tribeID].getType().getStartingUnit();
-                        Unit unit = Types.UNIT.createUnit(new Vector2d(i,j), 0, false, c.getActorId(), tribeID, unitType);
-                        board.addUnit(c, unit);
-
-                        //City tiles
-                        board.assignCityTiles(c, c.getBound());
-
-                        tribeID++;
                     }
+
+                    //Second character indicates the tribe for this tribe
+                    int tribeID = Integer.parseInt(tileSplit[1]);
+                    //Types.TRIBE tribe = Types.TRIBE.getTypeByKey(tribeID);
+
+                    //A city to create. Add it and assign it to the next tribe.
+                    City c = new City(i, j, tribeID);
+                    c.setCapital(true);
+                    board.addCityToTribe(c,rnd);
+
+                    //Also, each tribe starts with a unit in the same location where the city is
+                    Types.UNIT unitType = tribes[tribeID].getType().getStartingUnit();
+                    Unit unit = Types.UNIT.createUnit(new Vector2d(i,j), 0, false, c.getActorId(), tribeID, unitType);
+                    board.addUnit(c, unit);
+
+                    //City tiles
+                    board.assignCityTiles(c, c.getBound());
+
+                    tribesFound++;
                 }
 
                 board.setTerrainAt(i,j, Types.TERRAIN.getType(terrainChar));
 
-                if(tileSplit.length == 2)
+
+                if(!isCity && tileSplit.length == 2)
                 {
                     char resourceChar = tileSplit[1].charAt(0);
                     board.setResourceAt(i,j,Types.RESOURCE.getType(resourceChar));

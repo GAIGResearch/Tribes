b3adc96255a37f2a02dee4a78b57f9eede667d50
diegopliebana <diego.perez@qmul.ac.uk>
2020-05-21 18:23:07 +0100

    Tribes now read completely from the file and not needed on game initialization

--- src/core/game/LevelLoader.java
+++ src/core/game/LevelLoader.java
@@ -7,11 +7,11 @@ import core.actors.units.Unit;
 import utils.Vector2d;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Random;
 
 class LevelLoader
 {
-
     private Dimension size;
 
     LevelLoader()
@@ -21,17 +21,18 @@ class LevelLoader
 
     /**
      * Builds a level, receiving a file name.
-     * @param tribes tribes to play in this game
      * @param lines lines containing the level
      */
-    Board buildLevel(Tribe[] tribes, String[] lines, Random rnd) {
-
-        Board board = new Board();
+    Board buildLevel(String[] lines, Random rnd) {
 
         // Dimensions of the level read from the file.
         size.width = lines.length;
         size.height = lines.length;
-        int tribesFound = 0;
+
+        Tribe[] tribes = extractTribes(lines);
+        Board board = new Board();
+
+        int tribeID = 0;
         int numTribes = tribes.length;
 
         board.init(size.width, tribes);
@@ -48,49 +49,75 @@ class LevelLoader
                 // Retrieve the chars and assign the corresponding enum values in the board.
                 String[] tileSplit = tile[j].split(":");
                 char terrainChar = tileSplit[0].charAt(0);
-                boolean isCity = terrainChar == Types.TERRAIN.CITY.getMapChar();
 
-                if(isCity)
+                if(terrainChar == Types.TERRAIN.CITY.getMapChar())
                 {
-                    if(tribesFound==numTribes)
+                    if(tribeID==numTribes)
                     {
-                        //If we've already allocated all the cities to the number of tribes, turn this extra city into a village.
+                        //If we've already allocated all the cities to the number of tribes, turn this
+                        //extra city into a village.
                         terrainChar = Types.TERRAIN.VILLAGE.getMapChar();
+                    }else
+                    {
+                        //A city to create. Add it and assign it to the next tribe.
+                        City c = new City(i, j, tribeID);
+                        c.setCapital(true);
+                        board.addCityToTribe(c,rnd);
+
+                        //Also, each tribe starts with a unit in the same location where the city is
+                        Types.UNIT unitType = tribes[tribeID].getType().getStartingUnit();
+                        Unit unit = Types.UNIT.createUnit(new Vector2d(i,j), 0, false, c.getActorId(), tribeID, unitType);
+                        board.addUnit(c, unit);
+
+                        //City tiles
+                        board.assignCityTiles(c, c.getBound());
+
+                        tribeID++;
                     }
-
-                    //Second character indicates the tribe for this tribe
-                    int tribeID = Integer.parseInt(tileSplit[1]);
-                    //Types.TRIBE tribe = Types.TRIBE.getTypeByKey(tribeID);
-
-                    //A city to create. Add it and assign it to the next tribe.
-                    City c = new City(i, j, tribeID);
-                    c.setCapital(true);
-                    board.addCityToTribe(c,rnd);
-
-                    //Also, each tribe starts with a unit in the same location where the city is
-                    Types.UNIT unitType = tribes[tribeID].getType().getStartingUnit();
-                    Unit unit = Types.UNIT.createUnit(new Vector2d(i,j), 0, false, c.getActorId(), tribeID, unitType);
-                    board.addUnit(c, unit);
-
-                    //City tiles
-                    board.assignCityTiles(c, c.getBound());
-
-                    tribesFound++;
                 }
 
                 board.setTerrainAt(i,j, Types.TERRAIN.getType(terrainChar));
 
-
-                if(!isCity && tileSplit.length == 2)
+                if(tileSplit.length == 2)
                 {
                     char resourceChar = tileSplit[1].charAt(0);
                     board.setResourceAt(i,j,Types.RESOURCE.getType(resourceChar));
                 }
             }
         }
-
         return board;
     }
 
+    /**
+     * Extracts the tribes from the file lines initializing them
+     * @param lines information
+     * @return initialized array of tribes.
+     */
+    private Tribe[] extractTribes(String[] lines)
+    {
+        ArrayList<Types.TRIBE> tribes_list = new ArrayList<>();
+        for (int i = 0; i < size.height; ++i) {
+            String line = lines[i];
+            String[] tile = line.split(",");
+            for (String s : tile) {
+                String[] tileSplit = s.split(":");
+                char terrainChar = tileSplit[0].charAt(0);
+                boolean isCity = terrainChar == Types.TERRAIN.CITY.getMapChar();
+
+                if (isCity) {
+                    int tribeID = Integer.parseInt(tileSplit[1]);
+                    tribes_list.add(Types.TRIBE.getTypeByKey(tribeID));
+                }
+            }
+        }
+
+        Tribe[] tribesArray = new Tribe[tribes_list.size()];
+        int i = 0;
+        for(Types.TRIBE trType : tribes_list)
+        {
+            tribesArray[i++] = new Tribe(trType);
+        }
+        return tribesArray;
+    }
 
 }

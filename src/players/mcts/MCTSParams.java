f9b80c317a21d5e1e2043e24d6b2cefb33c43e79
diegopliebana <diego.perez@qmul.ac.uk>
2020-05-22 14:40:25 +0100

    Parameters for search methods

--- src/players/mcts/MCTSParams.java
+++ src/players/mcts/MCTSParams.java
@@ -2,36 +2,18 @@ package players.mcts;
 
 
 import javafx.util.Pair;
+import players.heuristics.AlgParams;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 @SuppressWarnings("WeakerAccess")
-public class MCTSParams {
-
-
-    public final int STOP_TIME = 0;
-    public final int STOP_ITERATIONS = 1;
-    public final int STOP_FMCALLS = 2;
-
-    public final int ENTROPY_HEURISTIC = 0;
-    public final int SIMPLE_HEURISTIC = 1;
-
-    public double epsilon = 1e-6;
+public class MCTSParams extends AlgParams {
 
     // Parameters
     public double K = Math.sqrt(2);
     public int rollout_depth = 10;//10;
-    public int heuristic_method = SIMPLE_HEURISTIC;
-
-    // Budget settings
-    public int stop_type = STOP_TIME;
-    public int num_iterations = 200;
-    public int num_fmcalls = 2000;
-    public int num_time = 40;
-
-    private int FORCE_TURN_END = 5;
 
     public void setParameterValue(String param, Object value) {
         switch(param) {
@@ -50,6 +32,7 @@ public class MCTSParams {
         return null;
     }
 
+
     public ArrayList<String> getParameters() {
         ArrayList<String> paramList = new ArrayList<>();
         paramList.add("K");
@@ -66,21 +49,4 @@ public class MCTSParams {
         return parameterValues;
     }
 
-    public Pair<String, ArrayList<Object>> getParameterParent(String parameter) {
-        return null;  // No parameter dependencies
-    }
-
-    public Map<Object, ArrayList<String>> getParameterChildren(String root) {
-        return new HashMap<>();  // No parameter dependencies
-    }
-
-    public Map<String, String[]> constantNames() {
-        HashMap<String, String[]> names = new HashMap<>();
-        names.put("heuristic_method", new String[]{"CUSTOM_HEURISTIC", "ADVANCED_HEURISTIC"});
-        return names;
-    }
-
-    public int getFORCE_TURN_END() {
-        return FORCE_TURN_END;
-    }
 }
